f651f47c251477485b3a1cec2901e8d657fd993f
Jason Gerlowski <gerlowskija@apache.org>
2023-01-05 15:14:30 -0500

    Tweak cluster-test template to use personal Solr fork

--- really-stressed.sh
+++ really-stressed.sh
@@ -1,19 +1,36 @@
 #!/bin/bash -x
 
-# First do a bunch of tests that contain my JAX-RS change
+# Performance test immediately preceding my JAX-RS change
+commit_hash=89adf62cf0902f351f65d12df2772d9bfa79327c
+for i in 1 100 200 300 400 500
+do
+  cat suites/cluster-test.json.template | sed s/@@COLL_LIMIT@@/${i}/ > suites/templated-cluster-test.json
+  ./cleanup.sh && ./stress.sh -c $commit_hash -s ${i}colls suites/templated-cluster-test.json 2>&1 | tee suites/results/${commit_hash}-${i}colls-stdout.txt
+done
+
+# Then get the results with the JAX-RS change
 commit_hash=62e3686d196ed0bb7a95bc2b25d6f4cd42d04bdb
-for i in 900 1000
-#for i in 800 900 1000
+for i in 1 100 200 300 400 500
 do
   cat suites/cluster-test.json.template | sed s/@@COLL_LIMIT@@/${i}/ > suites/templated-cluster-test.json
   ./cleanup.sh
   ./stress.sh -c $commit_hash -s ${i}colls suites/templated-cluster-test.json 2>&1 | tee suites/results/${commit_hash}-${i}colls-stdout.txt
 done
 
-# Then do the same tests on the commit immediately preceding my JAX-RS change
-commit_hash=89adf62cf0902f351f65d12df2772d9bfa79327c
-for i in 1 100 200 300 400 500 600 700 800 900 1000
+# Then do a commit which fixes some of the JAX-RS slowdown
+commit_hash=3e1b694a6bc9b1f010645d59cdc02f81e53813a4
+for i in 1 100 200 300 400 500
 do
   cat suites/cluster-test.json.template | sed s/@@COLL_LIMIT@@/${i}/ > suites/templated-cluster-test.json
-  ./cleanup.sh && ./stress.sh -c $commit_hash -s ${i}colls suites/templated-cluster-test.json 2>&1 | tee suites/results/${commit_hash}-${i}colls-stdout.txt
+  ./cleanup.sh
+  ./stress.sh -c $commit_hash -s ${i}colls suites/templated-cluster-test.json 2>&1 | tee suites/results/${commit_hash}-${i}colls-stdout.txt
+done
+
+# Last of all do a commit which hacks together the AppHandler sharing
+commit_hash=3e1b694a6bc9b1f010645d59cdc02f81e53813a4
+for i in 1 100 200 300 400 500
+do
+  cat suites/cluster-test.json.template | sed s/@@COLL_LIMIT@@/${i}/ > suites/templated-cluster-test.json
+  ./cleanup.sh
+  ./stress.sh -c $commit_hash -s ${i}colls suites/templated-cluster-test.json 2>&1 | tee suites/results/${commit_hash}-${i}colls-stdout.txt
 done
